650c16c5c190cecfb1d8dba64479f3d352ceb55e
morcodou <morcodou@gmail.com>
2020-10-06 00:24:25 -0400

    event hub

---
+++ Chapter05/ReceivingEventsToEventHub/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.EventHubs.Processor;
+using System;
+
+namespace ReceivingEventsToEventHub
+{
+    class Program
+    {
+        private const string EventHubConnectionString =
+                             "<your_event_hub_namespace_connection_string>";
+        private const string EventHubName = "<your_event_hub_name>";
+        private const string StorageContainerName = "<your_container_name>";
+        private const string StorageAccountName = "<your_storage_account_name>";
+        private const string StorageAccountKey = "<your_storage_account_access_key>";
+        private static readonly string StorageConnectionString = string.Format($"DefaultEndpointsProtocol=https;AccountName ={StorageAccountName};AccountKey={StorageAccountKey}");
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Registering EventProcessor...");
+
+            var eventProcessorHost = new EventProcessorHost(
+                EventHubName,
+                PartitionReceiver.DefaultConsumerGroupName,
+                EventHubConnectionString,
+                StorageConnectionString,
+                StorageContainerName);
+
+            // Registers the Event Processor Host and starts receiving messages
+            eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>();
+
+            Console.WriteLine("Receiving. Press ENTER to stop worker.");
+            Console.ReadLine();
+
+            // Disposes of the Event Processor Host
+            eventProcessorHost.UnregisterEventProcessorAsync();
+        }
+    }
+}
---
+++ Chapter05/SendingEventsToEventHub/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Azure.EventHubs;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendingEventsToEventHub
+{
+    class Program
+    {
+        private static EventHubClient eventHubClient;
+        private const string EventHubConnectionString = "<Your_event_hub_namespace_connection_string>";
+        private const string EventHubName = "<your_event_hub_name>";
+        private const int numMessagesToSend = 100;
+
+        static void Main(string[] args)
+        {
+            var connectionStringBuilder = new EventHubsConnectionStringBuilder(
+            EventHubConnectionString)
+            {
+                EntityPath = EventHubName
+            };
+
+            eventHubClient = EventHubClient.CreateFromConnectionString(
+            connectionStringBuilder.ToString());
+
+            for (var i = 0; i < numMessagesToSend; i++)
+            {
+                try
+                {
+                    var message = $"Message {i}";
+                    Console.WriteLine($"Sending message: {message}");
+                    eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(message)));
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"{DateTime.Now} > Exception: {exception.Message}");
+                }
+
+                Task.Delay(10);
+            }
+
+            Console.WriteLine($"{numMessagesToSend} messages sent.");
+
+            eventHubClient.CloseAsync();
+
+            Console.WriteLine("Press ENTER to exit.");
+            Console.ReadLine();
+        }
+    }
+}
